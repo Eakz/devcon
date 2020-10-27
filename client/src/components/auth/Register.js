f4df035e7a7c4ac9021efc278fe55f031cf2ad8e
EakzIT <pilpaff@gmail.com>
2020-10-27 20:20:58 +0200

    setting up routers/ refactoring html5 code

---
+++ client/src/components/auth/Register.js
@@ -0,0 +1,85 @@
+import React, { Fragment, useState } from "react";
+
+function Register() {
+    const [formData, setFormData] = useState({
+        name: "",
+        email: "",
+        password: "",
+        confpassword: "",
+    });
+    const { name, email, password, confpassword } = formData;
+    const handleChange = e =>
+        setFormData({ ...formData, [e.target.name]: [e.target.value] });
+    const handleSubmit = e => {
+        e.preventDefault();
+        if (password !== confpassword) {
+            console.log("Passwords no match");
+        } else {
+            console.log(formData);
+        }
+    };
+    return (
+        <Fragment>
+            <h1 className="large text-primary">Sign Up</h1>
+            <p className="lead">
+                <i className="fas fa-user"></i>Create Your Account
+            </p>
+            <form onSubmit={handleSubmit} className="form">
+                <div className="form-group">
+                    <input
+                        type="text"
+                        name="name"
+                        value={name}
+                        onChange={handleChange}
+                        placeholder="Name"
+                        id=""
+                        required
+                    />
+                </div>
+                <div className="form-group">
+                    <input
+                        type="email"
+                        name="email"
+                        placeholder="Email"
+                        value={email}
+                        onChange={handleChange}
+                    />
+                    <small className="form-text">
+                        This site uses Gravatar, so if you want a profile image,
+                        use a Gravatar email
+                    </small>
+                </div>
+                <div className="form-group">
+                    <input
+                        type="password"
+                        name="password"
+                        value={password}
+                        placeholder="Password"
+                        minlength="6"
+                        onChange={handleChange}
+                    />
+                </div>
+                <div className="form-group">
+                    <input
+                        type="password"
+                        name="confpassword"
+                        value={confpassword}
+                        placeholder="Confirm Password"
+                        minlength="6"
+                        onChange={handleChange}
+                    />
+                </div>
+                <input
+                    type="submit"
+                    value="Register"
+                    className="btn btn-primary"
+                />
+            </form>
+            <p className="my-1">
+                Already have an account? <a href="login.html">Sign in</a>
+            </p>
+        </Fragment>
+    );
+}
+
+export default Register;
