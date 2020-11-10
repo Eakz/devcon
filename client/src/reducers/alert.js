577b557b13f6c5bce62c0adae94b0b5b0b856417
EakzIT <pilpaff@gmail.com>
2020-11-10 21:32:07 +0200

    setting up Redux, adding uuid to dev deps

---
+++ client/src/reducers/alert.js
@@ -0,0 +1,13 @@
+import { SET_ALERT, CANCEL_ALERT } from "../actions/constants";
+const INITIAL_STATE = [];
+
+export default (state = INITIAL_STATE, { type, payload }) => {
+    switch (type) {
+        case SET_ALERT:
+            return [...state, payload];
+        case CANCEL_ALERT:
+            return state.filter(e => e.id !== payload);
+        default:
+            return state;
+    }
+};
